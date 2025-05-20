21dcc49034ea744a428ccd2e2d1dd386c0a9e61b
zhangxiaojuan <zhangxiaojuan@cambridgeig.com>
2025-05-20 15:05:19 +0800

    add file

---
+++ play.py
@@ -0,0 +1,44 @@
+import numpy as np
+import sys
+sys.path.append("game/")
+
+import pygame
+import wrapped_flappy_bird as game
+
+SOUND_ON = True
+game_state = game.GameState(30, SOUND_ON)
+
+currentScore = 0
+topScore = 0
+a_t = [1,0] # not flap
+FIRST_FRAME = True
+game.showScore(currentScore)
+
+terminal = False
+r_t = 0
+while True:
+    a_t = [1,0]
+    if FIRST_FRAME:
+    	game_state.getCurrentFrame()
+    	FIRST_FRAME = False		
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                a_t = [0,1]   
+    _, r_t, terminal = game_state.frame_step(a_t)     
+
+    if(r_t == 1):
+        currentScore += 1
+        game.showScore(currentScore)
+        topScore = max(topScore, currentScore)
+        print("Current Score: " + str(currentScore) + " Top Score: " + str(topScore))
+    
+    if terminal == True:
+        print("The bird died! You scored: " + str(currentScore))
+        print("##############################################")     
+        FIRST_FRAME = True
+        terminal = False
+        currentScore = 0
+        game.showScore(currentScore)
+				
